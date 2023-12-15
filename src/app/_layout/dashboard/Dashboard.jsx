623c721c2614b2c1192f21d58bf077fa923615fd
devtrills <godwinikechukwu.dev@gmail.com>
2023-12-15 12:42:48 +0100

    Update Dashboard.jsx

--- src/app/_layout/dashboard/Dashboard.jsx
+++ src/app/_layout/dashboard/Dashboard.jsx
@@ -7,7 +7,7 @@ const Dashboard = ({children}) => {
       <section className="h-screen w-screen overflow-x-hidden overflow-y-scroll">
         <section className="grid grid-cols-12 p-4 gap-3 w-full h-full">
           <Sidebar />
-          <div className="lg:col-span-10 md:col-span-11 col-span-12 bg-gray-100 rounded-lg p-4 overflow-x-hidden overflow-y-scroll w-full h-full shadow">
+          <div className="lg:col-span-10 md:col-span-11 col-span-11 bg-gray-100 rounded-lg p-4 overflow-x-hidden overflow-y-scroll w-full h-full shadow">
             {children}
           </div>
         </section>
