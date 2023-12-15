44f2e8af88a255a4dd05ca683c0c09486fa2b02a
devtrills <godwinikechukwu.dev@gmail.com>
2023-12-15 02:21:24 +0100

    fixe es lint

--- src/app/_containers/NotFound.jsx
+++ src/app/_containers/NotFound.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Image from "next/image";
 
+import img from "../../../public/404.gif"
+
 const NotFound = () => {
   return (
     <section className="fixed top-0 left-0 h-screen w-screen overflow-hidden flex justify-center items-center bg-white z-50">
       <Image
-        src="/404.gif"
+        src={img}
         alt="not found"
         loading="lazy"
         className="max-w-full"
