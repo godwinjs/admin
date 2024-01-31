75a8c965c87dc2b843b0b1cd4e5d86f5ab28048c
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-01-31 09:57:10 +0100

    Update AddNewProduct.jsx

--- src/app/_containers/dashboard/product/AddNewProduct.jsx
+++ src/app/_containers/dashboard/product/AddNewProduct.jsx
@@ -57,6 +57,7 @@ const AddNewProduct = () => {
     if(isReload){
       window.location.reload();
     }
+
   }, [isReload])
 
 
@@ -391,7 +392,7 @@ const AddNewProduct = () => {
                   placeholder="Separate by , or ⎵"
                   {...register("allOfSizes", { required: false })}
                   className={`w-full border-transparent focus:border-transparent focus:ring-transparent rounded-md bg-gray-200 p-2`}
-                  onKeyUp={(event) => {
+                  onKeyDown={(event) => {
                     console.log(event.key === " ")
                     if (event.key === " " || event.key === ",") { //event.which is depreciated
                       const sizesValue = event.target.value.replace(",", "");
